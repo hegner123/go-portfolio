24892f91884ea7c76405a4150da607e5f2f2be7a
Michael Hegner <hegner123@gmail.com>
2024-03-24 13:57:40 -0400

    updated install and vendor v

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/hegner123/go-portfolio
 
-go 1.20
+go 1.21
+
+toolchain go1.22.1
+
+require github.com/a-h/templ v0.2.646
--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-func main(){
-    print("hello")
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+)
+
+func main() {
+	component := hello("John")
+	
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on :3000")
+	http.ListenAndServe(":3000", nil)
 }
+
+
 
